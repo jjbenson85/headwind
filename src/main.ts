18bab9bcd8678dcb21fb26a0045e7b57bd7e4315
James Benson <jjbenson85@gmail.com>
2024-06-14 20:54:34 +0100

    feat: Add useNonUnitNumbers function for processing non-unit numbers in CSS properties

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,7 @@
 import { useHorizontalAndVerticalShorthand } from "./useHorizontalAndVerticalShorthand";
 import { useImportant } from "./useImportant";
+import { useNonUnitNumbers } from "./useNonUnitNumbers";
 import { useSimpleShorthand } from "./useSimpleShorthand";
-
-const useHandleNonUnitNumbers: PropertyProcessor = (items) => {
-  const lookup: string[] = ["flex"];
-  return items.map((item) => {
-    const key = lookup.find((e) => item.property === e);
-    if (!key) return item;
-
-    const vals = item.value.split("_");
-    const some = vals.some((val) => !isNaN(Number(val)));
-    if (!some) return item;
-
-    vals.forEach((val) => {
-      if (isNaN(Number(val))) return val;
-      item.rootVars.push(`--number-${val}: ${Number(val)};`);
-    });
-    const value = vals
-      .map((val) => {
-        if (isNaN(Number(val))) return val;
-        return `var(--number-${val})`;
-      })
-      .join("_");
-
-    return {
-      ...item,
-      value,
-    };
-  });
-};
 
 const useUnits: PropertyProcessor = (items) =>
   items.map((item) => {
@@ -226,7 +199,7 @@ function main(config?: { propertyProcessors: PropertyProcessor[] }): void {
     useImportant,
     useSimpleShorthand,
     useHorizontalAndVerticalShorthand,
-    useHandleNonUnitNumbers,
+    useNonUnitNumbers,
     useUnits,
     useTextSizes,
     useRoundedSizes,
---
+++ src/useNonUnitNumbers.ts
@@ -0,0 +1,29 @@
+import { PropertyProcessor } from "./main";
+
+export const useNonUnitNumbers: PropertyProcessor = (items) => {
+  const lookup: string[] = ["flex"];
+  return items.map((item) => {
+    const key = lookup.find((e) => item.property === e);
+    if (!key) return item;
+
+    const vals = item.value.split("_");
+    const some = vals.some((val) => !isNaN(Number(val)));
+    if (!some) return item;
+
+    vals.forEach((val) => {
+      if (isNaN(Number(val))) return val;
+      item.rootVars.push(`--number-${val}: ${Number(val)};`);
+    });
+    const value = vals
+      .map((val) => {
+        if (isNaN(Number(val))) return val;
+        return `var(--number-${val})`;
+      })
+      .join("_");
+
+    return {
+      ...item,
+      value,
+    };
+  });
+};
